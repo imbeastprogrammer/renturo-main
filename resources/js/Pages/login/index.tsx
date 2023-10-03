99af14570ee86885f9cf1c8819b4d340e80f86a2
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-10-03 13:16:04 +0800

    setup shadcn ui

--- resources/js/Pages/login/index.tsx
+++ resources/js/Pages/login/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/Components/ui/button";
 import { Link } from "@inertiajs/react";
 
 function LoginPage() {
     return (
         <div className="p-4">
             <Link href="/dashboard"> goto dashboard</Link>
+            <Button>Sample content</Button>
         </div>
     );
 }
