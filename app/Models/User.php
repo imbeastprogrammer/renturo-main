4e6ea906ad6b15f92dfa23e11a41f5f9f3e2227b
Zeus Villena <shunellezeusvillena.pci@gmail.com>
2023-10-09 16:52:06 +0800

    update tenant seeder with passport personal client install specific to tenant, setup laravel passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
@@ -39,7 +39,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'created_at',
         'updated_at',
         'deleted_at'
     ];
--- database/seeders/TenantSeeder.php
+++ database/seeders/TenantSeeder.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use App\Models\Central\Tenant;
 use App\Models\User;
+use Artisan;
+
 class TenantSeeder extends Seeder
 {
     /**
@@ -26,11 +28,24 @@ class TenantSeeder extends Seeder
             'domain' => 'main.' . config('tenancy.central_domains')[2]
         ]);
 
-        $tenant->run(function(){
+        $tenant->run(function () use ($tenant) {
             User::factory()->create([
                 'email' => 'admin@main.renturo.test',
                 'role' => User::ROLE_ADMIN
             ]);
+
+            User::factory()->create([
+                'email' => 'owner@main.renturo.test',
+                'role' => User::ROLE_OWNER
+            ]);
+
+            User::factory()->create([
+                'email' => 'user@main.renturo.test',
+                'role' => User::ROLE_USER
+            ]);
         });
+
+        //Passport install
+        Artisan::call("tenants:run passport:client --option='personal=personal' --option='name={$tenant->id}' --tenants={$tenant->id}");
     }
 }
