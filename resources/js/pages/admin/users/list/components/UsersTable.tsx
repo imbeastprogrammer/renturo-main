eec4d35eecaae6e7b413a0766a17c5e0c3d7b0b3
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-10-10 09:14:29 +0800

    build delete user modal

--- resources/js/pages/admin/users/list/components/UsersTable.tsx
+++ resources/js/pages/admin/users/list/components/UsersTable.tsx
@@ -9,6 +9,8 @@ import {
 import { EditIcon, EyeIcon, TrashIcon } from "lucide-react";
 import { User } from "@/types/users";
 import ActionMenu from "./ActionMenu";
+import DeleteUserModal from "./DeleteUserModal";
+import { useState } from "react";
 
 // const statusColor: Record<string, string> = {
 //     active: "#84C58A",
@@ -32,53 +34,67 @@ const menuItems = [
 ];
 
 function UsersTable({ users = [] }: UsersTableProps) {
+    const [deleteModalOpen, setDeletModalOpen] = useState(false);
+
     const handleMenuSelection = (value: string) => {
-        console.log(value);
+        switch (value) {
+            case MenuItems.DELETE:
+                return setDeletModalOpen(true);
+        }
     };
 
     return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead className="w-[100px]">Id</TableHead>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Email</TableHead>
-                    <TableHead>Date Joined</TableHead>
-                    <TableHead>Account</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead className="w-[50px]"></TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {users.map((user) => (
-                    <TableRow key={user.id}>
-                        <TableCell className="font-medium">{user.id}</TableCell>
-                        <TableCell>
-                            {[user.first_name, user.last_name].join(" ")}
-                        </TableCell>
-                        <TableCell>{user.email}</TableCell>
-                        <TableCell>NA for now</TableCell>
-                        <TableCell>{user.role}</TableCell>
-                        <TableCell>
-                            {/* <span
+        <>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Id</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Email</TableHead>
+                        <TableHead>Date Joined</TableHead>
+                        <TableHead>Account</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead className="w-[50px]"></TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {users.map((user) => (
+                        <TableRow key={user.id}>
+                            <TableCell className="font-medium">
+                                {user.id}
+                            </TableCell>
+                            <TableCell>
+                                {[user.first_name, user.last_name].join(" ")}
+                            </TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>NA for now</TableCell>
+                            <TableCell>{user.role}</TableCell>
+                            <TableCell>
+                                {/* <span
                                 className="capitalize"
                                 style={{ color: statusColor[user.status] }}
                             >
                                 {user.status}
 
                             </span> */}
-                            NA for now
-                        </TableCell>
-                        <TableCell>
-                            <ActionMenu
-                                menuItems={menuItems}
-                                onSelect={handleMenuSelection}
-                            />
-                        </TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+                                NA for now
+                            </TableCell>
+                            <TableCell>
+                                <ActionMenu
+                                    menuItems={menuItems}
+                                    onSelect={handleMenuSelection}
+                                />
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <DeleteUserModal
+                isOpen={deleteModalOpen}
+                onClose={() => setDeletModalOpen(false)}
+                userToDeleteId={1}
+            />
+        </>
     );
 }
 
