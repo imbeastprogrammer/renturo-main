3b9718387179d26fe405ddb8afb4d451e67d7b3f
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-10-24 10:25:40 +0800

    able to sort the designer component

--- resources/js/pages/admin/listings/form-builder/components/Dropzone/index.tsx
+++ resources/js/pages/admin/listings/form-builder/components/Dropzone/index.tsx
@@ -1,19 +1,15 @@
 import { v4 as uuidv4 } from 'uuid';
-import {
-    DragEndEvent,
-    useDndMonitor,
-    useDraggable,
-    useDroppable,
-} from '@dnd-kit/core';
+import { DragEndEvent, useDndMonitor, useDroppable } from '@dnd-kit/core';
 import {
     ElementsType,
     FormElementInstance,
     FormElements,
 } from '../FormElement';
 import useFormBuilder from '@/hooks/useFormBuilder';
+import { SortableContext, arrayMove, useSortable } from '@dnd-kit/sortable';
 
 function Dropzone() {
-    const { fields, addField } = useFormBuilder();
+    const { fields, addField, setFields } = useFormBuilder();
 
     const { setNodeRef } = useDroppable({
         id: 'designer-drop-area',
@@ -25,15 +21,58 @@ function Dropzone() {
     useDndMonitor({
         onDragEnd: (event: DragEndEvent) => {
             const { active, over } = event;
+            if (!active || !over) return;
+
+            const isToolboxItem = active?.data.current?.isToolboxItem;
+
             const isDesignerDroppingArea =
                 over?.data.current?.isDesignerDropArea;
 
-            if (isDesignerDroppingArea) {
+            if (isToolboxItem && isDesignerDroppingArea) {
                 const type = active.data?.current?.type;
                 const newField = FormElements[type as ElementsType].construct(
                     uuidv4(),
                 );
-                addField(fields.length, newField);
+                return addField(fields.length, newField);
+            }
+
+            // const isDroppingOverDesignerElementBottomHalf =
+            //     over.data?.current?.isBottomHalfDesignerElement;
+
+            // if (isToolboxItem && isDroppingOverDesignerElementBottomHalf) {
+            //     const type = active.data?.current?.type;
+            //     const newElement = FormElements[type as ElementsType].construct(
+            //         uuidv4(),
+            //     );
+
+            //     const overId = over.data?.current?.elementId;
+
+            //     const overElementIndex = fields.findIndex(
+            //         (el) => el.id === overId,
+            //     );
+            //     if (overElementIndex === -1) {
+            //         throw new Error('element not found');
+            //     }
+
+            //     let indexForNewElement = overElementIndex; // i assume i'm on top-half
+            //     if (isDroppingOverDesignerElementBottomHalf) {
+            //         indexForNewElement = overElementIndex + 1;
+            //     }
+
+            //     return addField(indexForNewElement, newElement);
+            // }
+
+            const isDraggingDesignerElement =
+                active.data?.current?.isDesignerElement;
+
+            if (isDraggingDesignerElement) {
+                const activeIdx = fields.findIndex(
+                    (field) => field.id === active.data.current?.elementId,
+                );
+                const overIdx = fields.findIndex(
+                    (field) => field.id === over.data.current?.elementId,
+                );
+                setFields(arrayMove(fields, activeIdx, overIdx));
             }
         },
     });
@@ -41,9 +80,14 @@ function Dropzone() {
     return (
         <div ref={setNodeRef} className='relative overflow-hidden'>
             <div className='hide-scrollbar relative h-full space-y-2 overflow-y-auto overflow-x-hidden bg-[#f4f4f4] p-8 shadow-lg'>
-                {fields.map((field) => (
-                    <DesignerElementWrapper key={field.id} element={field} />
-                ))}
+                <SortableContext items={fields}>
+                    {fields.map((field) => (
+                        <DesignerElementWrapper
+                            key={field.id}
+                            element={field}
+                        />
+                    ))}
+                </SortableContext>
             </div>
         </div>
     );
@@ -68,8 +112,8 @@ function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
         },
     });
 
-    const draggable = useDraggable({
-        id: element.id + '-drag-handler',
+    const sortable = useSortable({
+        id: element.id + '-sortable-handler',
         data: {
             type: element.type,
             elementId: element.id,
@@ -77,32 +121,25 @@ function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
         },
     });
 
-    if (draggable.isDragging) return null;
+    if (sortable.isDragging) return null;
 
     const DesignerElement = FormElements[element.type].designerComponent;
 
     return (
-        <>
-            <div ref={topHalf.setNodeRef} className='h-2 w-full rounded-t-md' />
-            <div
-                ref={draggable.setNodeRef}
-                {...draggable.listeners}
-                {...draggable.attributes}
-                className='relative'
-            >
-                {topHalf.isOver && (
-                    <div className='bg-primary absolute top-0 h-[7px] w-full rounded-md rounded-b-none' />
-                )}
-                <DesignerElement element={element} />
-                {bottomHalf.isOver && (
-                    <div className='bg-primary absolute bottom-0 h-[7px] w-full rounded-md rounded-t-none' />
-                )}
-                <div
-                    ref={bottomHalf.setNodeRef}
-                    className='h-2 w-full rounded-b-md'
-                />
-            </div>
-        </>
+        <div
+            ref={sortable.setNodeRef}
+            {...sortable.listeners}
+            {...sortable.attributes}
+            className='relative'
+        >
+            {topHalf.isOver && (
+                <div className='bg-primary absolute top-0 h-[7px] w-full rounded-md rounded-b-none' />
+            )}
+            <DesignerElement element={element} />
+            {bottomHalf.isOver && (
+                <div className='bg-primary absolute bottom-0 h-[7px] w-full rounded-md rounded-t-none' />
+            )}
+        </div>
     );
 }
 
