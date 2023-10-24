2db2f5eda23adbe4fb802c053500409216342307
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-10-24 13:34:06 +0800

    add properties editor on number input

--- resources/js/pages/admin/listings/form-builder/components/fields/NumberField.tsx
+++ resources/js/pages/admin/listings/form-builder/components/fields/NumberField.tsx
@@ -1,15 +1,45 @@
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { TrashIcon } from 'lucide-react';
 import { FormElement, FormElementInstance } from '../FormElement';
 import { Separator } from '@/components/ui/separator';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+} from '@/components/ui/form';
+import {
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Switch } from '@/components/ui/switch';
 import useFormBuilder from '@/hooks/useFormBuilder';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
 
 const extraAttributes = {
     is_required: false,
     label: 'Editable Label',
-    type: '',
+    type: 'number_input',
 };
+
+const schema = z.object({
+    is_required: z.boolean(),
+    label: z.string(),
+    type: z.string(),
+});
+
 const NumberField: FormElement = {
     type: 'number',
     construct: (id: string) => ({
@@ -50,8 +80,96 @@ function DesignerComponent({ element }: DesignerComponentProps) {
     );
 }
 
-function PropertiesComponent() {
-    return <div>TextField</div>;
+type PropertiesComponentProps = {
+    element: FormElementInstance;
+};
+function PropertiesComponent({ element }: PropertiesComponentProps) {
+    const { updateField } = useFormBuilder();
+    const form = useForm<z.infer<typeof schema>>({
+        defaultValues: element.extraAttributes,
+        resolver: zodResolver(schema),
+    });
+
+    const applyChanges = form.handleSubmit((values) => {
+        updateField(element.id, {
+            ...element,
+            extraAttributes: { ...values },
+        });
+    });
+
+    return (
+        <AccordionItem value={element.id} className='border-0'>
+            <AccordionTrigger className='mb-2 rounded-lg bg-white p-3 px-4'>
+                {element.type}
+            </AccordionTrigger>
+            <AccordionContent>
+                <Form {...form}>
+                    <form
+                        onBlur={applyChanges}
+                        onSubmit={applyChanges}
+                        className='space-y-2'
+                    >
+                        <FormField
+                            name='is_required'
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem className='flex items-center justify-between space-y-0 rounded-lg bg-white px-4 py-3'>
+                                    <FormLabel>Required</FormLabel>
+                                    <FormControl>
+                                        <Switch
+                                            checked={field.value}
+                                            onCheckedChange={field.onChange}
+                                        />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name='type'
+                            control={form.control}
+                            render={({ field }) => {
+                                return (
+                                    <FormItem className='rounded-lg bg-white px-4 py-3'>
+                                        <FormLabel>Type</FormLabel>
+                                        <FormControl>
+                                            <Select
+                                                {...field}
+                                                onValueChange={field.onChange}
+                                            >
+                                                <SelectTrigger>
+                                                    <SelectValue />
+                                                </SelectTrigger>
+                                                <SelectContent>
+                                                    <SelectItem value='number_input'>
+                                                        Number Input
+                                                    </SelectItem>
+                                                    <SelectItem value='mobile_number_input'>
+                                                        Mobile Number
+                                                    </SelectItem>
+                                                </SelectContent>
+                                            </Select>
+                                        </FormControl>
+                                    </FormItem>
+                                );
+                            }}
+                        />
+                        <FormField
+                            name='label'
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem className='rounded-lg bg-white px-4 py-3'>
+                                    <FormLabel>Label</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
+            </AccordionContent>
+        </AccordionItem>
+    );
 }
 
 export default NumberField;
