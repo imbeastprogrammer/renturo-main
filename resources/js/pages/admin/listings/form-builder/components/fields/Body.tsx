3ecfc1b3cfe77042ec9fdb7bd70e4a3f65d5ca07
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-03 13:25:14 +0800

    add two new field body and heading

---
+++ resources/js/pages/admin/listings/form-builder/components/fields/Body.tsx
@@ -0,0 +1,151 @@
+import _ from 'lodash';
+import { z } from 'zod';
+import {
+    ElementsType,
+    FormElement,
+    FormElementInstance,
+    FormElements,
+} from '../FormElement';
+import { Separator } from '@/components/ui/separator';
+import { Label } from '@/components/ui/label';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+} from '@/components/ui/form';
+import {
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Textarea } from '@/components/ui/textarea';
+
+import { DeleteIcon } from '@/assets/form-builder';
+import useFormBuilder from '@/hooks/useFormBuilder';
+import useFieldTypes from '../../hooks/useFieldTypes';
+import FieldTypeChanger from '../FieldTypeChanger';
+import PropertyEditorHandle from '../PropertyEditorHandle';
+
+const extraAttributes = {
+    text: 'Please enter a text',
+};
+
+const schema = z.object({ text: z.string() });
+
+const Body: FormElement = {
+    type: 'body',
+    construct: (id) => ({
+        id,
+        type: 'body',
+        extraAttributes,
+    }),
+    designerComponent: DesignerComponent,
+    propertiesComponent: PropertiesComponent,
+};
+
+type DesignerComponentProps = {
+    element: FormElementInstance;
+};
+function DesignerComponent({ element }: DesignerComponentProps) {
+    const { removeField, setSelectedField, updateField, current_page_id } =
+        useFormBuilder();
+    const elementInstance = element as FormElementInstance & {
+        extraAttributes: typeof extraAttributes;
+    };
+
+    const { fieldTypes, currentFieldType } = useFieldTypes(element.type);
+
+    const handleValueChange = (value: ElementsType) => {
+        updateField(
+            current_page_id,
+            element.id,
+            FormElements[value].construct(element.id),
+        );
+    };
+
+    return (
+        <div className='w-full' onClick={() => setSelectedField(element)}>
+            <div className='flex justify-between'>
+                <FieldTypeChanger
+                    icon={currentFieldType?.icon}
+                    value={currentFieldType?.type}
+                    data={fieldTypes}
+                    onValueChange={handleValueChange}
+                />
+                <button
+                    onClick={() => removeField(current_page_id, element.id)}
+                >
+                    <DeleteIcon />
+                </button>
+            </div>
+            <Separator className='my-2' />
+            <div className='pointer-events-none space-y-2'>
+                <Label className='text-[20px] font-normal'>
+                    {elementInstance.extraAttributes.text}
+                </Label>
+            </div>
+        </div>
+    );
+}
+
+type PropertiesComponentProps = {
+    element: FormElementInstance;
+};
+
+function PropertiesComponent({ element }: PropertiesComponentProps) {
+    const { updateField, current_page_id } = useFormBuilder();
+    const form = useForm<z.infer<typeof schema>>({
+        defaultValues: element.extraAttributes,
+        resolver: zodResolver(schema),
+    });
+
+    const { currentFieldType } = useFieldTypes(element.type);
+
+    const applyChanges = form.handleSubmit((values) => {
+        updateField(current_page_id, element.id, {
+            ...element,
+            extraAttributes: { ...values },
+        });
+    });
+
+    return (
+        <AccordionItem value={element.id} className='border-0'>
+            <AccordionTrigger className='mb-2 rounded-lg bg-white p-3 px-4'>
+                {currentFieldType && (
+                    <PropertyEditorHandle
+                        type={currentFieldType?.title}
+                        icon={currentFieldType.icon}
+                    />
+                )}
+            </AccordionTrigger>
+            <AccordionContent>
+                <Form {...form}>
+                    <form
+                        onBlur={applyChanges}
+                        onSubmit={applyChanges}
+                        className='space-y-2'
+                    >
+                        <FormField
+                            name='text'
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem className='rounded-lg bg-white px-4 py-3'>
+                                    <FormLabel>Label</FormLabel>
+                                    <FormControl>
+                                        <Textarea {...field} />
+                                    </FormControl>
+                                </FormItem>
+                            )}
+                        />
+                    </form>
+                </Form>
+            </AccordionContent>
+        </AccordionItem>
+    );
+}
+
+export default Body;
