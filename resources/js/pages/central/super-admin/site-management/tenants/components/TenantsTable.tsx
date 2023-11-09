5bcf43659a6a66e5ddc0e2c88dcc2dc7620e286b
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-09 14:54:21 +0800

    remove deletion on tenant

--- resources/js/pages/central/super-admin/site-management/tenants/components/TenantsTable.tsx
+++ resources/js/pages/central/super-admin/site-management/tenants/components/TenantsTable.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { router } from '@inertiajs/react';
 import { MoreHorizontalIcon } from 'lucide-react';
 import {
@@ -17,83 +16,60 @@ import {
 } from '@/components/ui/dropdown-menu';
 
 import { Tenant } from '@/types/tenant';
-import DeleteTenantsModal from './DeleteTenantsModal';
 
 type TenantsTableProps = {
     tenants: Tenant[];
 };
 
 function TenantsTable({ tenants }: TenantsTableProps) {
-    const [deleteModalState, setDeleteModalState] = useState({
-        id: '',
-        isOpen: false,
-    });
-
-    const openDeleteUserModal = (id: string) =>
-        setDeleteModalState({ id, isOpen: true });
-
     const navigateToEditPage = (id: string) =>
         router.visit(`/super-admin/site-management/tenants/edit/${id}`);
 
     return (
-        <>
-            <Table>
-                <TableHeader>
-                    <TableRow className='text-base text-[#2E3436]'>
-                        <TableHead>ID</TableHead>
-                        <TableHead>First Name</TableHead>
-                        <TableHead>Last Name</TableHead>
-                        <TableHead>Email</TableHead>
-                        <TableHead>Domain</TableHead>
-                        <TableHead className='text-right'>Action</TableHead>
+        <Table>
+            <TableHeader>
+                <TableRow className='text-base text-[#2E3436]'>
+                    <TableHead>ID</TableHead>
+                    <TableHead>First Name</TableHead>
+                    <TableHead>Last Name</TableHead>
+                    <TableHead>Email</TableHead>
+                    <TableHead>Domain</TableHead>
+                    <TableHead className='text-right'>Action</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {tenants.map((tenant) => (
+                    <TableRow
+                        key={tenant.id}
+                        className='text-base text-[#2E3436]/50'
+                    >
+                        <TableCell className='font-medium'>
+                            {tenant.id}
+                        </TableCell>
+                        <TableCell>tenant</TableCell>
+                        <TableCell>tenant</TableCell>
+                        <TableCell>Static</TableCell>
+                        <TableCell>Static</TableCell>
+                        <TableCell className='text-right'>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <MoreHorizontalIcon />
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className='-translate-x-4 font-outfit'>
+                                    <DropdownMenuItem
+                                        onClick={() =>
+                                            navigateToEditPage(tenant.id)
+                                        }
+                                    >
+                                        Edit Tenant
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </TableCell>
                     </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {tenants.map((tenant) => (
-                        <TableRow
-                            key={tenant.id}
-                            className='text-base text-[#2E3436]/50'
-                        >
-                            <TableCell className='font-medium'>
-                                {tenant.id}
-                            </TableCell>
-                            <TableCell>tenant</TableCell>
-                            <TableCell>tenant</TableCell>
-                            <TableCell>Static</TableCell>
-                            <TableCell>Static</TableCell>
-                            <TableCell className='text-right'>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger>
-                                        <MoreHorizontalIcon />
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent className='-translate-x-4 font-outfit'>
-                                        <DropdownMenuItem
-                                            onClick={() =>
-                                                navigateToEditPage(tenant.id)
-                                            }
-                                        >
-                                            Edit Tenant
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem
-                                            onClick={() =>
-                                                openDeleteUserModal(tenant.id)
-                                            }
-                                        >
-                                            Delete Tenant
-                                        </DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-            <DeleteTenantsModal
-                isOpen={deleteModalState.isOpen}
-                id={deleteModalState.id}
-                onClose={() => setDeleteModalState({ id: '', isOpen: false })}
-            />
-        </>
+                ))}
+            </TableBody>
+        </Table>
     );
 }
 
