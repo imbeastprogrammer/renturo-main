2411946935e60f42f441c9775a29dc79af50e7ed
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-10 14:30:15 +0800

    build tenant usermangement owner delete dialog

---
+++ resources/js/assets/tenant/index.tsx
@@ -0,0 +1,1 @@
+export { default as DeleteWarningIcon } from './delete-warning-icon.png';
--- resources/js/pages/tenants/admin/user-management/owners/components/OwnersTable.tsx
+++ resources/js/pages/tenants/admin/user-management/owners/components/OwnersTable.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
     Table,
     TableBody,
@@ -14,54 +15,75 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { router } from '@inertiajs/react';
+import DeleteOwnerModal from './DeleteOwnerModal';
 
 function OwnersTable() {
+    const [deleteModalState, setDeleteModalState] = useState({
+        isOpen: false,
+        id: 0,
+    });
+
     const navigateToUpdatePage = (id: number) =>
         router.visit(`/admin/user-management/owners/update/${id}?active=Users`);
 
     return (
-        <Table>
-            <TableHeader>
-                <TableRow className='text-base font-normal text-[#2E3436]'>
-                    <TableHead className='w-[100px]'>ID</TableHead>
-                    <TableHead>First Name</TableHead>
-                    <TableHead>Last Name</TableHead>
-                    <TableHead>Email</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Created At</TableHead>
-                    <TableHead>Last Update</TableHead>
-                    <TableHead className='text-right'>Action</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                <TableRow className='text-base font-normal text-[#2E3436]/50'>
-                    <TableCell className='font-medium'>1</TableCell>
-                    <TableCell>Joshua</TableCell>
-                    <TableCell>Dela Cruz</TableCell>
-                    <TableCell>kamotekid.dev@gmail.com</TableCell>
-                    <TableCell>Active</TableCell>
-                    <TableCell>10-27-23 16:50:32</TableCell>
-                    <TableCell>10-27-23 16:50:32</TableCell>
-                    <TableCell className='text-right'>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger>
-                                <MoreHorizontalIcon />
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent className='-translate-x-6'>
-                                <DropdownMenuItem
-                                    onClick={() => navigateToUpdatePage(1)}
-                                >
-                                    Edit
-                                </DropdownMenuItem>
-                                <DropdownMenuItem className='text-red-500'>
-                                    Delete
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </TableCell>
-                </TableRow>
-            </TableBody>
-        </Table>
+        <>
+            <Table>
+                <TableHeader>
+                    <TableRow className='text-base font-normal text-[#2E3436]'>
+                        <TableHead className='w-[100px]'>ID</TableHead>
+                        <TableHead>First Name</TableHead>
+                        <TableHead>Last Name</TableHead>
+                        <TableHead>Email</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Created At</TableHead>
+                        <TableHead>Last Update</TableHead>
+                        <TableHead className='text-right'>Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow className='text-base font-normal text-[#2E3436]/50'>
+                        <TableCell className='font-medium'>1</TableCell>
+                        <TableCell>Joshua</TableCell>
+                        <TableCell>Dela Cruz</TableCell>
+                        <TableCell>kamotekid.dev@gmail.com</TableCell>
+                        <TableCell>Active</TableCell>
+                        <TableCell>10-27-23 16:50:32</TableCell>
+                        <TableCell>10-27-23 16:50:32</TableCell>
+                        <TableCell className='text-right'>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <MoreHorizontalIcon />
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className='-translate-x-6'>
+                                    <DropdownMenuItem
+                                        onClick={() => navigateToUpdatePage(1)}
+                                    >
+                                        Edit
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem
+                                        className='text-red-500'
+                                        onClick={() =>
+                                            setDeleteModalState({
+                                                isOpen: true,
+                                                id: 1,
+                                            })
+                                        }
+                                    >
+                                        Delete
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            <DeleteOwnerModal
+                isOpen={deleteModalState.isOpen}
+                id={deleteModalState.id}
+                onClose={() => setDeleteModalState({ isOpen: false, id: 0 })}
+            />
+        </>
     );
 }
 
