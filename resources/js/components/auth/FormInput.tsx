8cd610884171427c184bf6c273655f9f8364819f
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-15 10:01:10 +0800

    update login page to v2

---
+++ resources/js/components/auth/FormInput.tsx
@@ -0,0 +1,64 @@
+import { LucideIcon } from 'lucide-react';
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '../ui/form';
+import { Input, InputProps } from '../ui/input';
+import { Control, FieldValues, Path } from 'react-hook-form';
+import { cn } from '@/lib/utils';
+
+type FormInputProps<T> = {
+    label?: string;
+    control: Control<FieldValues & T>;
+    name: Path<FieldValues & T>;
+    icon?: LucideIcon;
+    showError?: boolean;
+} & InputProps;
+
+function FormInput<T>({
+    control,
+    name,
+    icon: Icon,
+    showError = true,
+    ...props
+}: FormInputProps<T>) {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field, fieldState }) => (
+                <FormItem className='w-full'>
+                    {props.label && (
+                        <FormLabel className='text-[18px] font-medium'>
+                            {props.label}
+                        </FormLabel>
+                    )}
+                    <FormControl>
+                        <div className='relative flex items-center gap-2'>
+                            <Input
+                                className={cn(
+                                    'rounded-none border-x-0 border-b border-t-0 p-6 pr-16 text-lg placeholder:text-black/50 placeholder:text-gray-400 focus-visible:ring-transparent',
+                                    {
+                                        'border-red-500 bg-red-500/5 text-red-500':
+                                            !!fieldState.error?.message,
+                                    },
+                                )}
+                                {...field}
+                                {...props}
+                            />
+                            {Icon && (
+                                <Icon className='absolute right-6 text-gray-400' />
+                            )}
+                        </div>
+                    </FormControl>
+                    {showError && <FormMessage />}
+                </FormItem>
+            )}
+        />
+    );
+}
+
+export default FormInput;
