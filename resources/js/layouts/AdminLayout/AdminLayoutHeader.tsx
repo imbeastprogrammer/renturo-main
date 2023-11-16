c9f34ed51a6ce8acb96e888c55819ec2d618acbd
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-16 10:49:10 +0800

    add breadcrum on admin

--- resources/js/layouts/AdminLayout/AdminLayoutHeader.tsx
+++ resources/js/layouts/AdminLayout/AdminLayoutHeader.tsx
@@ -1,9 +1,19 @@
 import UserButton from '@/components/tenant/UserButton';
+import Breadcrumb from './Breadcrumb';
+import { LabelMap } from '.';
 
 function AdminLayoutHeader() {
+    const { pathname } = window.location;
+
     return (
         <header>
-            <div className='flex items-center justify-end gap-4'>
+            <div className='flex items-center justify-between gap-4 pb-4'>
+                <div>
+                    <h1 className='text-[30px] font-semibold'>
+                        {LabelMap[pathname]}
+                    </h1>
+                    <Breadcrumb />
+                </div>
                 <UserButton />
             </div>
         </header>
