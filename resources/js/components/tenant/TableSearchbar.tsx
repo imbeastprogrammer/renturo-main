5db7a4a365b4ab756a27565e6139fce798a93c6c
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-20 16:56:14 +0800

    build owner promotions page

--- resources/js/components/tenant/TableSearchbar.tsx
+++ resources/js/components/tenant/TableSearchbar.tsx
@@ -1,10 +1,10 @@
-import { SearchIcon } from 'lucide-react';
+import { FaSearch } from 'react-icons/fa';
 import { Input, InputProps } from '../ui/input';
 
 function TableSearchbar(props: InputProps) {
     return (
         <div className='relative flex w-full items-center'>
-            <SearchIcon className='absolute left-2 text-black/20' />
+            <FaSearch className='absolute left-2 text-black/20' />
             <Input
                 className='pl-10 placeholder:text-black/20 focus-visible:ring-transparent'
                 {...props}
