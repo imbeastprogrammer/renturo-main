d7df768d2b90de9d7587389a766b9f30ba4e3893
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-21 11:29:02 +0800

    refactor/admin/phone-number-input

--- resources/js/assets/tenant/form/index.tsx
+++ resources/js/assets/tenant/form/index.tsx
@@ -1,2 +1,3 @@
 export { default as ErrorIcon } from './error.png';
 export { default as SuccessIcon } from './success.png';
+export { default as PhFlag } from './ph-flag.png';
--- resources/js/components/forms/FormPhoneNumberInput.tsx
+++ resources/js/components/forms/FormPhoneNumberInput.tsx
@@ -6,25 +6,27 @@ import {
     FormLabel,
     FormMessage,
 } from '../ui/form';
-import { Input, InputProps } from '../ui/input';
+import { IMaskInput } from 'react-imask';
 import { Control, FieldValues, Path } from 'react-hook-form';
 import { cn } from '@/lib/utils';
+import { PhFlag } from '@/assets/tenant/form';
 
-type FormInputProps<T> = {
+type FormPhoneNumberInput<T> = {
     label?: string;
     control: Control<FieldValues & T>;
     name: Path<FieldValues & T>;
     icon?: LucideIcon;
     showError?: boolean;
-} & InputProps;
+    placeholder?: string;
+};
 
-function FormInput<T>({
+function FormPhoneNumberInput<T>({
     control,
     name,
     icon: Icon,
     showError = true,
     ...props
-}: FormInputProps<T>) {
+}: FormPhoneNumberInput<T>) {
     return (
         <FormField
             control={control}
@@ -38,15 +40,31 @@ function FormInput<T>({
                     )}
                     <FormControl>
                         <div className='relative flex items-center gap-2'>
-                            <Input
+                            <div className='absolute left-4 flex items-center gap-2'>
+                                <img
+                                    src={PhFlag}
+                                    alt='Philippine flag'
+                                    className='h-[25px] object-contain'
+                                />
+                                <span className='text-black/50'>+63</span>
+                            </div>
+                            <IMaskInput
+                                mask='000 0000 000'
+                                value={field.value}
                                 className={cn(
-                                    'h-[60px] rounded-lg bg-[#F3F7FD] p-6 pr-16 text-base placeholder:text-black/50 placeholder:text-gray-400 focus-visible:ring-transparent',
+                                    'h-[60px] w-full rounded-lg border bg-[#F3F7FD] pl-24 pr-16 text-base outline-none placeholder:text-black/50 placeholder:text-gray-400 focus-visible:ring-transparent',
                                     {
                                         'border-red-500 bg-red-500/5 text-red-500':
                                             !!fieldState.error?.message,
                                     },
                                 )}
-                                {...field}
+                                onAccept={(value) => {
+                                    const newValue = value.replace(
+                                        /(\s+|\+63)/g,
+                                        '',
+                                    );
+                                    field.onChange(newValue);
+                                }}
                                 {...props}
                             />
                             {Icon && (
@@ -61,4 +79,4 @@ function FormInput<T>({
     );
 }
 
-export default FormInput;
+export default FormPhoneNumberInput;
