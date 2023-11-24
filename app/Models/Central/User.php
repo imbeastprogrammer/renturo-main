a1611feb06ce1ac16c36c806d13246f57abea61b
Jayson Lacson <ceo@kabootekph.com>
2023-11-24 15:36:14 +0800

    Update Tenant

--- app/Models/Central/User.php
+++ app/Models/Central/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name',
         'last_name',
+        'username',
         'role',
         'email',
         'mobile_number',
