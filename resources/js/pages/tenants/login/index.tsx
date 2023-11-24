5affba62564e8541094c46148ad726ccf329545b
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-24 14:54:27 +0800

    update hero sizing and padding on login page

--- resources/js/pages/tenants/login/index.tsx
+++ resources/js/pages/tenants/login/index.tsx
@@ -5,11 +5,11 @@ import LoginForm from './components/LoginForm';
 function LoginPage() {
     return (
         <div className='grid h-screen grid-cols-[610px_1fr] place-items-center gap-8'>
-            <div className='relative grid h-full w-full place-items-center bg-metalic-blue p-4'>
+            <div className='relative grid h-full w-full place-items-center bg-metalic-blue p-8'>
                 <img
                     src={RenturoTextLogoBlue}
                     alt='app logo'
-                    className='absolute left-4 top-4 h-[45px] object-contain'
+                    className='absolute left-8 top-8 h-[45px] object-contain'
                 />
                 <div className='grid place-items-center gap-8'>
                     <img src={LoginHero} alt='login hero' />
