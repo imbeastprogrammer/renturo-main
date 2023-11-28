3c16c85fda0131f4e9bd21a320b17268f6a0f008
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-28 11:01:17 +0800

    bugfixed: paginated response causes crash on user management admins section on tenant

--- resources/js/pages/tenants/admin/user-management/admins/index.tsx
+++ resources/js/pages/tenants/admin/user-management/admins/index.tsx
@@ -1,5 +1,5 @@
-import { Link } from '@inertiajs/react';
-import { ReactNode, useState } from 'react';
+import { Link, router } from '@inertiajs/react';
+import { ReactNode } from 'react';
 import { PlusIcon } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
@@ -11,10 +11,32 @@ import TableSearchbar from '@/components/tenant/TableSearchbar';
 import Pagination from '@/components/tenant/Pagination';
 
 type AdminsProps = {
-    admins: User[];
+    admins: PaginatedAdmin;
 };
+
+type PaginatedAdmin = {
+    current_page: number;
+    data: User[];
+    prev_page_url: string | null;
+    next_page_url: string | null;
+    last_page: number;
+};
+
 function Admins({ admins }: AdminsProps) {
-    const [currentPage, setCurrentPage] = useState(1);
+    const { pathname } = window.location;
+    const recordsCount = admins.data.length;
+
+    const handleNextPage = () => {
+        if (admins.next_page_url) router.replace(admins.next_page_url);
+    };
+
+    const handlePrevPage = () => {
+        if (admins.prev_page_url) router.replace(admins.prev_page_url);
+    };
+
+    const handlePageChange = (page: number) => {
+        router.replace(pathname, { data: { page } });
+    };
 
     return (
         <div className='grid grid-rows-[auto_1fr_auto] gap-4 rounded-xl border bg-white p-4 shadow-lg'>
@@ -39,25 +61,26 @@ function Admins({ admins }: AdminsProps) {
                     </Link>
                 </div>
             </div>
-            <AdminsTable admins={admins} />
+            <AdminsTable admins={admins.data} />
             <div className='flex items-center justify-between'>
-                <div className='text-sm'>
-                    <span>Showing 1 to 3 of 3 users</span>
+                <div className='text-[15px] font-medium text-black/50'>
+                    Showing {recordsCount} record(s) of page{' '}
+                    {admins.current_page}
                 </div>
                 <Pagination
-                    currentPage={currentPage}
-                    numberOfPages={10}
-                    onNextPage={(page) => setCurrentPage(page + 1)}
-                    onPrevPage={(page) => setCurrentPage(page - 1)}
-                    onPageChange={(page) => setCurrentPage(page)}
+                    currentPage={admins.current_page}
+                    numberOfPages={admins.last_page}
+                    onNextPage={handleNextPage}
+                    onPrevPage={handlePrevPage}
+                    onPageChange={handlePageChange}
                 />
                 <div className='flex items-center gap-2 text-sm'>
                     <span>Page</span>
                     <Input
-                        value={currentPage || 1}
+                        value={admins.current_page}
                         className='h-8 w-16 text-center'
                         type='number'
-                        onChange={(e) => setCurrentPage(Number(e.target.value))}
+                        readOnly
                     />
                     <span>of {10}</span>
                 </div>
