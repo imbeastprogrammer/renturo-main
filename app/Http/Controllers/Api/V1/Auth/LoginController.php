416d400299ba47691de6dbeb46a678ff9147ee78
Jayson Lacson <ceo@kabootekph.com>
2024-04-27 08:24:34 +0800

    Update login controller tenant response

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -14,43 +14,38 @@ class LoginController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        try {
-            $verificationCode = rand(1000, 9999);
+        $verificationCode = rand(1000, 9999);
 
-            $request->authenticate();
+        $request->authenticate();
 
-            $user = $request->user();
+        $user = $request->user();
 
-            $accessToken = $user->createToken('personal-access-token')->accessToken;
+        $accessToken = $user->createToken('personal-access-token')->accessToken;
 
-            #TODO: check what is the purpose of mobileVerification()->mobile_number?
-            
-            $user->mobileVerification()->create([
-                // 'mobile_number' => $user->mobileVerification()->mobile_number,
-                'mobile_number' => $user->mobile_number,
-                'code' => $verificationCode,
-                'expires_at' => Carbon::now()->addSeconds(300),
-            ]);
+        #TODO: check what is the purpose of mobileVerification()->mobile_number?
+        
+        $user->mobileVerification()->create([
+            // 'mobile_number' => $user->mobileVerification()->mobile_number,
+            'mobile_number' => $user->mobile_number,
+            'code' => $verificationCode,
+            'expires_at' => Carbon::now()->addSeconds(300),
+        ]);
 
-            // TODO: replace this email sending to mobile sending, temporary medium for sending verification code
-            // TODO: remove the verification code from the response. 
-            // TODO: when user is not authenticated, the user model should be removed. 
+        // TODO: replace this email sending to mobile sending, temporary medium for sending verification code
+        // TODO: remove the verification code from the response. 
+        // TODO: when user is not authenticated, the user model should be removed. 
 
-            Mail::to($user->email)->send(new SendMobileVerificationCode(['code' => $verificationCode]));
+        Mail::to($user->email)->send(new SendMobileVerificationCode(['code' => $verificationCode]));
 
-            return response()->json([
-                'message' => 'success',
-                'body' => [
-                    'message' => 'Verification code was sent to your mobile number.',
-                    'user' => $user,
-                    'verification_code' => $verificationCode, // return temporary in response
-                    'access_token' => $accessToken
-                ]
-            ], 201);
-
-        } catch (\Exception $e) { 
-            Log::error($e->getMessage());
-        }
+        return response()->json([
+            'message' => 'success',
+            'body' => [
+                'message' => 'Verification code was sent to your mobile number.',
+                'user' => $user,
+                'verification_code' => $verificationCode, // return temporary in response
+                'access_token' => $accessToken
+            ]
+        ], 201);
     }
 
     public function logout()
